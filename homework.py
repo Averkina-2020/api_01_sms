73e8a0631d0f46b47984c1cac540b70ae13c96a7
Аверина Ольга <olga_averkina2000@mail.ru>
2021-02-03 17:19:08 +0300

    corrections after the review

--- homework.py
+++ homework.py
@@ -1,3 +1,4 @@
+import logging
 import os
 import time
 
@@ -10,33 +11,37 @@ from twilio.rest import Client
 load_dotenv()
 
 
-account_sid_twilio = os.getenv('ACCOUNT_SID_TWILIO')
-token_twilio = os.getenv('TOKEN_TWILIO')
-client = Client(account_sid_twilio, token_twilio)
+ACCOUNT_SID_TWILIO = os.getenv('ACCOUNT_SID_TWILIO')
+TOKEN_TWILIO = os.getenv('TOKEN_TWILIO')
+client = Client(ACCOUNT_SID_TWILIO, TOKEN_TWILIO)
 
-token_vk = os.getenv('TOKEN_VK')
-BASE_URL_VK = 'https://api.vk.com/method/users.get'
+TOKEN_VK = os.getenv('TOKEN_VK')
+BASE_URL_VK = 'https://api.vk.com'
 API_V = '5.92'
 
-number_from = os.getenv('NUMBER_FROM')
-number_to = os.getenv('NUMBER_TO')
+NUMBER_FROM = os.getenv('NUMBER_FROM')
+NUMBER_TO = os.getenv('NUMBER_TO')
 
 
 def get_status(user_id):
     params = {
         'user_ids': user_id,
         'v': API_V,
-        'access_token': token_vk,
+        'access_token': TOKEN_VK,
         'fields': 'online'
     }
-    status = requests.post(BASE_URL_VK, params=params)
-    return status.json()['response'][0]['online']
+    url = '{}/method/users.get'.format(BASE_URL_VK)
+    try:
+        status = requests.post(url=url, params=params)
+        return status.json()['response'][0]['online']
+    except Exception:
+        logging.exception('Error')
 
 
 def send_sms(sms_text):
     message = client.messages.create(
-        to=number_to,
-        from_=number_from,
+        to=NUMBER_TO,
+        from_=NUMBER_FROM,
         body=sms_text
     )
     return message.sid
